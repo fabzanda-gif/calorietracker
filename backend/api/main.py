c601ad2175dd5e6bc45a5bd9d19a76418d404206
fabzanda-gif <fab.zanda@gmail.com>
2026-08-22 19:26:36 +0200

    Add recipes router to FastAPI application

--- backend/api/main.py
+++ backend/api/main.py
@@ -5,6 +5,7 @@ from backend.api.routers.meals import router as meals_router
 from backend.api.routers.activities import router as activities_router
 from backend.api.routers.weight import router as weight_router
 from backend.api.routers.daily_logs import router as daily_logs_router
+from backend.api.routers.recipes import router as recipes_router
 
 
 app = FastAPI(
@@ -17,3 +18,4 @@ app.include_router(meals_router)
 app.include_router(activities_router)
 app.include_router(weight_router)
 app.include_router(daily_logs_router)
+app.include_router(recipes_router)
